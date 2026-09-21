ca398b759a0c6fb00714261c95042b26ba92c6c6
Ayyub ansaari <ayyubansaari5@gmail.com>
2026-09-21 22:06:32 +0530

    Remove repayment management methods from interface
    Removed methods for adding, retrieving, updating, and deleting repayments from the RepaymentService interface.

--- src/main/java/com/loanmanagement/service/RepaymentService.java
+++ src/main/java/com/loanmanagement/service/RepaymentService.java
@@ -3,11 +3,5 @@ package com.loanmanagement.service;
 import com.loanmanagement.model.Repayment;
 
 public interface RepaymentService {
-    void addRepayment(Repayment repayment);
-
-    Repayment getRepaymentById(int repaymentId);
-
-    void updateRepayment(Repayment repayment);
-
-    void deleteRepayment(int repaymentId);
+    
 }
