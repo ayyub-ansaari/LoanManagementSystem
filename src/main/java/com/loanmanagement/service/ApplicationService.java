675c5ff28c4b0f919cf29d865f66721314cd3c7b
Ayyub ansaari <ayyubansaari5@gmail.com>
2026-09-21 22:08:30 +0530

    Remove application management methods from interface
    Removed methods for managing loan applications.

--- src/main/java/com/loanmanagement/service/ApplicationService.java
+++ src/main/java/com/loanmanagement/service/ApplicationService.java
@@ -3,11 +3,5 @@ package com.loanmanagement.service;
 import com.loanmanagement.model.LoanApplication;
 
 public interface ApplicationService {
-    void addApplication(LoanApplication application);
 
-    LoanApplication getApplicationById(int applicationId);
-
-    void updateApplication(LoanApplication application);
-
-    void deleteApplication(int applicationId);
 }
