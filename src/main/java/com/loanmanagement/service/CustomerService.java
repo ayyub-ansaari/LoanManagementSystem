b144b26b7dfcbca82ff2be2c4da60f541fb28ba7
Ayyub ansaari <ayyubansaari5@gmail.com>
2026-09-21 22:05:01 +0530

    Update CustomerService.java

--- src/main/java/com/loanmanagement/service/CustomerService.java
+++ src/main/java/com/loanmanagement/service/CustomerService.java
@@ -3,11 +3,5 @@ package com.loanmanagement.service;
 import com.loanmanagement.model.Customer;
 
 public interface CustomerService {
-    void addCustomer(Customer customer);
-
-    Customer getCustomerById(int customerId);
-
-    void updateCustomer(Customer customer);
-
-    void deleteCustomer(int customerId);
+    
 }
