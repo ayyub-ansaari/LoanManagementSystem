2c22c21cfb8276052a820e9ebee689e39d556a19
Ayyub ansaari <ayyubansaari5@gmail.com>
2026-09-21 22:06:52 +0530

    Remove loan type CRUD methods from interface
    Removed CRUD methods from LoanTypeService interface.

--- src/main/java/com/loanmanagement/service/LoanTypeService.java
+++ src/main/java/com/loanmanagement/service/LoanTypeService.java
@@ -3,11 +3,5 @@ package com.loanmanagement.service;
 import com.loanmanagement.model.LoanType;
 
 public interface LoanTypeService {
-    void addLoanType(LoanType loanType);
-
-    LoanType getLoanTypeById(int loanTypeId);
-
-    void updateLoanType(LoanType loanType);
-
-    void deleteLoanType(int loanTypeId);
+   
 }
