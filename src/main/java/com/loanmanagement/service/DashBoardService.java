1a0b4afc96af87c744f36d53c9d1c5a2271244b6
Ayyub ansaari <ayyubansaari5@gmail.com>
2026-09-21 22:07:33 +0530

    Remove unused methods from DashBoardService interface
    Removed methods for retrieving total customers, loans, loan amount, and repayment amount.

--- src/main/java/com/loanmanagement/service/DashBoardService.java
+++ src/main/java/com/loanmanagement/service/DashBoardService.java
@@ -1,11 +1,5 @@
 package com.loanmanagement.service;
 
 public interface DashBoardService {
-    int getTotalCustomers();
 
-    int getTotalLoans();
-
-    double getTotalLoanAmount();
-
-    double getTotalRepaymentAmount();
 }
