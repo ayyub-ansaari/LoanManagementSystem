f3b29347eba2e519fef67f085f306294d5a9675e
ayyub-ansaari <ayyubansaari5@gmail.com>
2026-09-24 11:51:55 +0530

    loantypeservice progress

--- src/main/java/com/loanmanagement/service/impl/LoanTypeServiceImpl.java
+++ src/main/java/com/loanmanagement/service/impl/LoanTypeServiceImpl.java
@@ -1,11 +1,21 @@
 package com.loanmanagement.service.impl;
 
+import com.loanmanagement.dao.LoanTypeDao;
 import com.loanmanagement.model.LoanType;
 import com.loanmanagement.service.LoanTypeService;
+import com.loanmanagement.model.Role;
 
 public class LoanTypeServiceImpl implements LoanTypeService {
+
+    private final LoanTypeDao loanTypeDao;
+
+    public LoanTypeServiceImpl(LoanTypeDao loanTypeDao){
+        this.loanTypeDao = loanTypeDao;
+    }
+
     @Override
     public void addLoanType(LoanType loanType) {
+        requiredAdmin();
 
     }
 
